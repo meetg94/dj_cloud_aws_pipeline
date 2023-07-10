d7d527539ab55a01b82f7d9ee534e46cf678287f
Meet Guleria <meetguleria94@gmail.com>
2023-07-09 19:15:22 -0700

    first2

---
+++ basicpage/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def home(request):
+    template = loader.get_template('homepage.html')
+    return HttpResponse(template.render())
